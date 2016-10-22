58eafaa764b5691b1bbf30942f7a38fbeab1b6b5
zthxxx <zth_9451@qq.com>
2016-10-23 03:13:52 +0800

    1) Reset baidu token and fixed _baidu_oauth_conf.

--- UnitTest/BaiduSpeechApiTest/ReadLocalBaiduToken.py
+++ UnitTest/BaiduSpeechApiTest/ReadLocalBaiduToken.py
@@ -4,9 +4,9 @@ from BaiduSpeech.SpeechRecognizer import *
 
 
 _baidu_oauth_conf = 'BaiduSpeech/BaiduOAuth.ini'
-# api_key, secret_key = get_baidu_api_key_config(baidu_oauth_conf)
+# api_key, secret_key = get_baidu_api_key_config(_baidu_oauth_conf)
 # access_token = get_baidu_token_url(api_key, secret_key)
-# save_baidu_token_config(baidu_oauth_conf, access_token)
+# save_baidu_token_config(_baidu_oauth_conf, access_token)
 _BAIDU_ACCESS_TOKEN = get_baidu_token_config(_baidu_oauth_conf)
 if not _BAIDU_ACCESS_TOKEN:
     _BAIDU_ACCESS_TOKEN = os.getenv('BAIDU_ACCESS_TOKEN')
